c74c046e8e5f85e1b75d35b46cb8433ebe4a3693
Mahmoud-K-Nassar <thkilerboyyyy@gmail.com>
2025-11-14 05:54:37 +0200

    starting with movie details

--- app/src/main/java/com/example/movieapplication/model/celebrity.kt
+++ app/src/main/java/com/example/movieapplication/model/celebrity.kt
@@ -6,6 +6,9 @@ import com.google.gson.annotations.SerializedName
 data class Celebrity(
     val id: Int,                      // 🆔 رقم تعريف المشهور
     val name: String?,                // 👤 اسم المشهور
+    val birthday: String?,
+    val place_of_birth: String?,
+    val biography: String?,
     @SerializedName("known_for_department") val role: String?, // 🎭 المجال اللي مشهور فيه (تمثيل، إخراج، إلخ)
     @SerializedName("profile_path") val profilePath: String?   // 🖼️ رابط صورة المشهور
 )
